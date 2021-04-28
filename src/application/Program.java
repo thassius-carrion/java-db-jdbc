46bd9631e53c542e7d66ca1d7e9527f83a49b459
THASSIUS <thassius.carrion@hotmail.com>
2021-04-28 20:24:00 -0300

    trabalhando com Statement e ResultSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,8 +12,30 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("select * from department"); //comando SQL, o resultado da consulta por st, estou adicionando no resultSet
+			
+			while(rs.next()){
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("name")); //nesta row do resultSet, queremos o int da coluna Id e o String da coluna name
+			}
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeRs();
+			DB.closeSt();
+			DB.closeConnection();
+		}
 
 	}
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -46,4 +48,27 @@ public class DB {
 			throw new DbException(e.getMessage());
 		}
 	}
+	
+	public static void closeSt(Statement st) {
+		if(st != null) {
+			try {
+					st.close();
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}	
+		}
+	}
+	
+	public static void closeRs(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
 }
