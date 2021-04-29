72902f2bce2c447fb583e5e3bbae833d96141d47
THASSIUS <thassius.carrion@hotmail.com>
2021-04-29 15:59:48 -0300

    atualizando dados no DB

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,48 +14,28 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		
-		PreparedStatement st = null;
+		PreparedStatement st = null; 
 		
 		try {
 			conn = DB.getConnection();
 			
 			st = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? " //quero mudar o salario 
+					+ "WHERE " //de quem? qual minha restricao?
+					+ "(DepartmentId = ?)"); //atualizando os salarios de todos os funcionarios de certo departamento
 			
-			st.setString(1, "Karl Purple");
-			st.setString(2, "karl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/07/1996").getTime()));
-			st.setDouble(4, 3000.00);
-			st.setInt(5, 4);
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
 			
-			int rowsAffected = st.executeUpdate(); //quando vamos alterar dados, chamamos executeUpdate, resultado é um numero int indicando qyuantas linhas foram alteradas
+			int rowsAffected = st.executeUpdate();
 			
-			if(rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while(rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done, Id = " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected.");
-			}
-			
+			System.out.println("Done! Rows affected: "+ rowsAffected);
 		}
-		catch (SQLException e) {
+		catch(SQLException e){
 			e.printStackTrace();
 		}
-		catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
 		finally {
 			DB.closeSt(st);
 			DB.closeConnection();
